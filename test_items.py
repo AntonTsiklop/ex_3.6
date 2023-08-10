f4d75d733be86e7fa72097ca4f3a5dc1dacb4795
Anton <tsikalo.tsikalo@mail.ru>
2023-08-10 23:44:54 +0300

    example 3.6

---
+++ test_items.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+
+
+def test_presence_add_to_cart_button(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    assert bool(browser.find_elements(By.CSS_SELECTOR, "#add_to_basket_form > button")), 'Нет кнопки "Добавить в корзину"'
+
